c0a400c72ff839d071847eca1f535b7abce62a6c
Sean C Davis <scdavis41@gmail.com>
2018-04-10 16:11:09 -0400

    Render contentful data via templates

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ activate :contentful do |f|
   f.space         = { contentful: '6xtqh9zzbhsk'}
   f.access_token  = ENV['CONTENTFUL_ACCESS_TOKEN']
   f.cda_query     = { limit: 1000 }
-  f.content_types = { post: 'post', category: 'category', author: 'authors' }
+  f.content_types = { posts: 'post', categories: 'category', authors: 'authors' }
 end
 
 # Layouts
@@ -27,6 +27,20 @@ page '/*.txt', layout: false
 
 # Proxy pages
 # https://middlemanapp.com/advanced/dynamic-pages/
+
+data.contentful.posts.each do |_, post|
+  date = post.published_date
+  path = "/#{date.year}/#{date.strftime('%m')}/#{date.strftime('%d')}/#{post.slug}.html"
+  proxy path, "/templates/post.html", locals: { post: post }, ignore: true
+end
+
+data.contentful.authors.each do |_, author|
+  proxy "/authors/#{author.slug}.html", "/templates/author.html", locals: { author: author }, ignore: true
+end
+
+data.contentful.categories.each do |_, category|
+  proxy "/topics/#{category.slug}.html", "/templates/category.html", locals: { category: category }, ignore: true
+end
 
 # proxy(
 #   '/this-page-has-no-template.html',
