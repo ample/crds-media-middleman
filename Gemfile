f62438def25d36b1d66fdc6d11233142f1428766
Sean C Davis <scdavis41@gmail.com>
2018-04-11 09:42:18 -0400

    Add pagination

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'middleman', '~> 4.2'
 gem 'middleman-autoprefixer', '~> 2.7'
 
+gem 'activesupport'
 gem 'contentful_middleman'
 gem 'dotenv-rails', groups: [:development, :test]
 
--- config.rb
+++ config.rb
@@ -1,4 +1,7 @@
 require 'dotenv/load'
+require 'active_support/core_ext/array'
+
+POSTS_PER_PAGE = 2
 
 # ---------------------------------------- | Extensions
 
@@ -27,6 +30,10 @@ data.contentful.posts.each do |_, post|
   proxy path, "/templates/post.html", locals: { post: post }, ignore: true
 end
 
+data.contentful.posts.to_a.in_groups_of(POSTS_PER_PAGE).each_with_index do |posts, idx|
+  proxy "/page/#{idx + 1}/index.html", "/templates/posts_page.html", locals: { page_num: idx + 1, posts: posts }, ignore: true
+end
+
 data.contentful.authors.each do |_, author|
   proxy "/authors/#{author.slug}/index.html", "/templates/author.html", locals: { author: author }, ignore: true
 end
@@ -52,8 +59,28 @@ helpers do
     "/categories/#{category.slug}/"
   end
 
+  def post_page_path(page_num)
+    "/page/#{page_num}/"
+  end
+
   def markdown(text)
     Kramdown::Document.new(text).to_html
+  end
+
+  def posts_per_page
+    POSTS_PER_PAGE
+  end
+
+  def pagination_links(current_page = 1)
+    total_posts = data.contentful.posts.size
+    total_pages = (total_posts.to_f / POSTS_PER_PAGE).ceil
+    page_nums = (current_page - 2 .. current_page + 2).to_a.reject { |i| i <= 0 || i > total_pages }
+    partial 'partials/pagination', locals: {
+      current_page: current_page,
+      page_nums: page_nums,
+      prev_link: current_page > 1,
+      next_link: current_page < total_pages
+    }
   end
 
 end
